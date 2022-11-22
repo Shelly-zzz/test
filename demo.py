04bf23a37d91314fe36571a6be96cbb67740eb78
shelly <13715044944@163.com>
2022-11-22 22:00:06 +0800

    2200

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
 print("This is a test demo")
 print("Line added by Shelly")
+<<<<<<< HEAD
 print("updata maybe conflict on 21:32")
+=======
+print("add test on 21:04")
+>>>>>>> d18ac46 (commit comment on 21:12)
